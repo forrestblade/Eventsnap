f290080b398c305437b77f8a991686be3b4b6c63
Forrest Blade <forrestblade.code@gmail.com>
2018-04-09 08:52:01 -0700

    forntend headway

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatIconModule, MatCardModule, MatToolbarModule, MatSidenavModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatCardModule, MatToolbarModule, MatSidenavModule, MatDatepickerModule, MatFormFieldModule, MatNativeDateModule, MatInputModule, MatAutocompleteModule, MatSliderModule, MatCheckboxModule, MatMenuModule, MatExpansionModule, MatRadioModule } from '@angular/material';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { SearchComponent } from './search/search.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    SearchComponent
+    SearchComponent,
+    LandingpageComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +33,19 @@ import { AppRoutingModule } from './/app-routing.module';
     MatToolbarModule,
     MatSidenavModule,
     BrowserAnimationsModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    FormsModule,
+    MatSliderModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatExpansionModule,
+    MatRadioModule
 
   ],
   providers: [],
