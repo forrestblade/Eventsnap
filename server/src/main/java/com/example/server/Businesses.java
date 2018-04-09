eadfad0a48590443c9a9bbf4e9b7a7b00b5dfea9
Brandon Welner <brandonmaxwelner@gmail.com>
2018-04-09 11:37:51 -0700

    added auto-generated id's

--- server/src/main/java/com/example/server/Businesses.java
+++ server/src/main/java/com/example/server/Businesses.java
@@ -1,11 +1,20 @@
 package com.example.server;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 @Entity
+@JsonIdentityInfo(
+		generator = ObjectIdGenerators.PropertyGenerator.class,
+		property="id")
 public class Businesses {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String business_type;
--- server/src/main/java/com/example/server/UserPlan.java
+++ server/src/main/java/com/example/server/UserPlan.java
@@ -4,12 +4,21 @@ import java.sql.Time;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 @Entity
+@JsonIdentityInfo(
+		generator = ObjectIdGenerators.PropertyGenerator.class,
+		property="id")
 public class UserPlan {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Long budget_id;
 	private Long location_id;
