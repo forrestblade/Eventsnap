d2130be00681f481a81c243e225d2e1a6b3c0e00
Lindsay Aichinger <lindsayaichinger@Lindsays-MacBook-Pro.local>
2018-04-11 11:20:18 -0700

    Changing users

--- client/src/app/users.ts
+++ client/src/app/users.ts
@@ -3,8 +3,5 @@ export class Users {
     id: number;
     username: String;
     email_address: String;
-<<<<<<< HEAD
-=======
     password: String;
->>>>>>> 65f46dee9566d1eceefaeb89692d08b52941e537
 }
