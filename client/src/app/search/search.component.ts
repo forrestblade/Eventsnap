892ce01c18cf3e374b2f2581888622cbd1e58a04
Forrest <forrestblade.code@gmail.com>
2018-04-23 15:28:26 -0700

    Search UI

--- client/src/app/search/search.component.ts
+++ client/src/app/search/search.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Events } from '../events';
 import { EventService } from '../event.service';
+import { UserPlan } from '../userplan';
+import { Events } from '../events';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-search',
@@ -9,44 +10,121 @@ import { EventService } from '../event.service';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  constructor(public eventService: EventService) {}
 
-  // constructor(private eventService: EventService) {}
+  buttonName: String = "Map View"
 
-  myControl: FormControl = new FormControl();
+  userPlan: Array<UserPlan>
+  model: any = {};
+  loading = false;
+  public someRange: number[] = [3, 7];
+  public tooltipSlider = document.getElementById('slider-tooltips')
+  public sliderTime_min;
+  public sliderTime_max;
+  public show:boolean = false;
 
-  options = [
-    '4am-12pm',
-    '12pm-4pm',
-    '4pm-11pm',
-    '11pm-3am'
-   ];
 
-   step = 0;
-   events: Array<Events>
-   model: any = {};
+  events: Array<Events>
 
-   loading = false;
+
+  toggle() {
+    this.show = !this.show;
+
+    // CHANGE THE NAME OF THE BUTTON.
    
-   addEvent(events: Events){
-     console.log(events)
-    //  this.eventService.addEvent(event).subscribe();
-   }
-
-  setStep(index: number) {
-    this.step = index;
   }
 
-  nextStep() {
-    this.step++;
+  addEvent(events: Events){
+    console.log(events)
+    this.eventService.addEvent(events).subscribe();
   }
 
-  prevStep() {
-    this.step--;
+  getEvents() {
+    this.eventService.getEvents()
+    .subscribe(data => this.events = data);
   }
 
-  constructor() { }
+
+
+  convertToTimeMin(someRange){
+    if (someRange[0] == 0 || someRange[0] == 24){
+      this.sliderTime_min = "12:00 AM";
+    } else if (someRange[0] == 12) {
+      this.sliderTime_min = "12:00 PM"
+    } else if (someRange[0] == 12.5) {
+      this.sliderTime_min = "12:30 PM"
+    } else if (someRange[0] == 0.5) {
+      this.sliderTime_min = "12:30 AM"
+    } else if ((someRange[0] < 13) && (someRange[0] % 1 == 0)) {
+      this.sliderTime_min = (someRange[0] +":00 AM");
+    } else if (someRange[0] < 13) {
+      this.sliderTime_min = ((someRange[0] - 0.5) +":30 AM");
+    } else if ((someRange[0] >= 13) && (someRange[0] % 1 == 0)) {
+      this.sliderTime_min = ((someRange[0] - 12) + ":00 PM");
+    } else if (someRange[0] >= 13) {
+      this.sliderTime_min = ((someRange[0] - 12.5) + ":30 PM");
+    } 
+    return this.sliderTime_min;
+    
+  }
+
+  convertToTimeMax(someRange){
+    if (someRange[1] == 0 || someRange[1] == 24){
+      this.sliderTime_max = "12:00 AM";
+    } else if (someRange[1] == 0.5) {
+      this.sliderTime_max = "12:30 AM"
+    } else if (someRange[1] == 12) {
+      this.sliderTime_max = "12:00 PM"
+    } else if (someRange[1] == 12.5) {
+      this.sliderTime_max = "12:30 PM"
+    } else if ((someRange[1] < 13) && (someRange[1] % 1 == 0)) {
+      this.sliderTime_max = (someRange[1] +":00 AM");
+    } else if (someRange[1] < 13) {
+      this.sliderTime_max = ((someRange[1] - 0.5) +":30 AM");
+    } else if ((someRange[1] >= 13) && (someRange[1] % 1 == 0)) {
+      this.sliderTime_max = ((someRange[1] - 12) + ":00 PM");
+    } else if (someRange[1] >= 13) {
+      this.sliderTime_max = ((someRange[1] - 12.5) + ":30 PM");
+    } 
+    return this.sliderTime_max;
+  }
+
+  
+
+
+    addUserPlan(userPlan: UserPlan){
+      console.log(userPlan)
+      this.eventService.addUserPlan(userPlan).subscribe();
+    }
+  
+    getUserPlan() {
+      this.eventService.getUserPlan()
+      .subscribe(data => this.userPlan = data);
+    }
+
+    // setStartTime(sliderTime_min) {
+    //   this.model.start_time = moment(this.sliderTime_min, ["hh:mm a"]).format("HH:mm:ss");
+    // }
+    
+    // setEndTime(sliderTime_max) {
+    //   this.model.end_time = moment(this.sliderTime_max, ["hh:mm a"]).format("HH:mm:ss");
+    // }
+
+    onSubmit() {
+      this.loading = true;
+      this.model.end_time = moment(this.sliderTime_max, ["hh:mm a"]).format("HH:mm:ss");
+      this.model.start_time = moment(this.sliderTime_min, ["hh:mm a"]).format("HH:mm:ss");        
+      
+      
+    }
+    
+    
+    
+  
 
   ngOnInit() {
+      this.getUserPlan();
+      this.getEvents();
   }
 
 }
